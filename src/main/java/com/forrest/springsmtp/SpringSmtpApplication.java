886ee3d740668bfc87e4ddc36505fa6a8afc7495
daluplo <lsdalupan@github.com>
2017-08-28 17:16:29 +0800

    add sample code

---
+++ src/main/java/com/forrest/springsmtp/SpringSmtpApplication.java
@@ -0,0 +1,12 @@
+package com.forrest.springsmtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSmtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSmtpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/forrest/springsmtp/SpringSmtpTests.java
@@ -0,0 +1,16 @@
+package com.forrest.springsmtp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSmtpTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
